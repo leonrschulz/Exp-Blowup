59be8cd2d560c1bc2ca8904f0ebbf5e229483a7a
Martin Desharnais-Schäfer <desharnais-schaefer@ifi.lmu.de>
2026-06-15 22:14:07 +0200

    Use shorter syntax following Tobias Nipkow's comment

--- CNF_DNF_Exp_Blowup/CNF_DNF_Exp_Blowup.thy
+++ CNF_DNF_Exp_Blowup/CNF_DNF_Exp_Blowup.thy
@@ -15,9 +15,10 @@ lemma card_Domain_le:
   using assms by (metis card_image_le fst_eq_Domain)
 
 (*TODO: Delete when a new Isabelle version is released. *)
+thm card_le_card_if_mem_imp_ex_mem
 lemma card_le_card_if_mem_imp_ex_mem:
   fixes f :: "'a \<Rightarrow> 'b \<Rightarrow> 'c" and \<X> :: "'a set" and \<Y> :: "'c set"
-  defines "XY \<equiv> {(x, y)| x y. x \<in> \<X> \<and> f x y \<in> \<Y>}"
+  defines "XY \<equiv> {(x, y). x \<in> \<X> \<and> f x y \<in> \<Y>}"
   assumes "finite \<X>" and "finite \<Y>" and
     f_inj: "inj_on (\<lambda>(x, y). f x y) XY" and
     ex_in_\<Y>: "\<And>x. x \<in> \<X> \<Longrightarrow> \<exists>y. f x y \<in> \<Y>"
